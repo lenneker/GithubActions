369eee1f224da3307b15e9b0d2e77cbd84886c6e
lenneker <lenneker_2006@hotmail.com>
2022-03-28 20:30:49 +0000

    asdf

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,8 +3,8 @@ const github = require("@actions/github");
 
 try{
 //throw( new Error("Some error message"));
-const name = core.getInput("who-to-greet");
-console.log('Hello ${name}');
+const name = core.getInput(`who-to-greet`);
+console.log(`Hello ${name}`);
 
 const time = new Date();
 core.setOutput("time", time.toTimeString());
