d519429d8d68fea49fc7b57f2cd12b1007cc40eb
Lenneker Nascimento <56844930+lenneker@users.noreply.github.com>
2022-04-18 11:03:35 -0300

    Update index.js

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,8 +3,15 @@ const github = require("@actions/github");
 
 try{
 //throw( new Error("Some error message"));
-const name = core.getInput(`who-to-greet`);
-console.log(`Hello ${name}`);
+//const name = core.getInput(`who-to-greet`);
+//console.log(`Hello ${name}`);
+    
+ 
+  const comidaPrincipal = core.getInput('prato');
+  const bebida = core.getInput('bebida');
+  console.log(`voce vai comer hoje ${comidaPrincipal} e vai beber ${bebida}!`);
+    
+    
 
 const time = new Date();
 core.setOutput("time", time.toTimeString());
